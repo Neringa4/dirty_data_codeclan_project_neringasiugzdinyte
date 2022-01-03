f5081aa47e194fbb979bd4392ef7a476f7e7c325
Neringa4 <94367447+Neringa4@users.noreply.github.com>
2022-01-03 11:36:50 +0000

    Clean column names

--- task1/data_cleaning_scripts/cleaning_script.R
+++ task1/data_cleaning_scripts/cleaning_script.R
@@ -1,3 +1,10 @@
 library(tidyverse)
 
+
 raw_decathlon_data <- readr::read_rds("raw_data/decathlon.rds")
+
+
+clean_names_decathlon <- raw_decathlon_data %>% 
+  janitor::clean_names() %>% 
+  rename_with(str_remove, pattern = "x")
+
