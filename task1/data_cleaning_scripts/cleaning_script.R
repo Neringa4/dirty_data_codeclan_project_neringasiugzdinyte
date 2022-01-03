35e21ad3b3bb8be8be279d58d2f797469e423514
Neringa4 <94367447+Neringa4@users.noreply.github.com>
2022-01-03 11:37:31 +0000

    Convert row names to a column

--- task1/data_cleaning_scripts/cleaning_script.R
+++ task1/data_cleaning_scripts/cleaning_script.R
@@ -8,3 +8,7 @@ clean_names_decathlon <- raw_decathlon_data %>%
   janitor::clean_names() %>% 
   rename_with(str_remove, pattern = "x")
 
+
+athlete_names_decathlon <- clean_names_decathlon %>% 
+  rownames_to_column("athlete_name")
+  
