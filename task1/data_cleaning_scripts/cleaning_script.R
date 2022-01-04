d38b1b9db9d71cac2ca2dca16fdfaeb6607e249a
Neringa4 <94367447+Neringa4@users.noreply.github.com>
2022-01-04 16:00:51 +0000

    Fix cleaning_script.R

--- task1/data_cleaning_scripts/cleaning_script.R
+++ task1/data_cleaning_scripts/cleaning_script.R
@@ -1,12 +1,11 @@
 library(tidyverse)
 
 
-raw_decathlon_data <- readr::read_rds("raw_data/decathlon.rds")
+raw_decathlon_data <- read_rds("raw_data/decathlon.rds")
 
 
 clean_names_decathlon <- raw_decathlon_data %>% 
-  janitor::clean_names() %>% 
-  rename_with(str_remove, pattern = "x")
+  janitor::clean_names()
 
 
 athlete_names_decathlon <- clean_names_decathlon %>% 
