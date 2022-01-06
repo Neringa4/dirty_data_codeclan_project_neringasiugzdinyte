a45cfc02317e50622f149b907a0082da67b2bb00
Neringa4 <94367447+Neringa4@users.noreply.github.com>
2022-01-06 09:49:00 +0000

    Add comments to cleaning_script.R

--- task1/data_cleaning_scripts/cleaning_script.R
+++ task1/data_cleaning_scripts/cleaning_script.R
@@ -4,9 +4,16 @@ library(tidyverse)
 raw_decathlon_data <- read_rds("raw_data/decathlon.rds")
 
 
+
+# Cleaned column names
+
 clean_names_decathlon <- raw_decathlon_data %>% 
   janitor::clean_names()
 
+
+
+# Transferred row names into a column for easier analysis, since it is 
+# part of the data and cleaned athlete names.
 
 athlete_names_decathlon <- clean_names_decathlon %>% 
   rownames_to_column("athlete_name") %>% 
@@ -14,8 +21,5 @@ athlete_names_decathlon <- clean_names_decathlon %>%
   mutate(athlete_name = str_replace(athlete_name, "Mcmullen", "McMullen"))
 
 
-clean_decathlon_data <- athlete_names_decathlon
-  
 
-write_csv(clean_decathlon_data,
-          "clean_data/decathlon_data.csv")
+write_csv(athlete_names_dacathlon, "clean_data/decathlon_data.csv")
