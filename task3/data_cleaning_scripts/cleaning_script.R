0c0d8e4e985fb5ebaa04cc57264c6bdcc3f3db54
Neringa4 <94367447+Neringa4@users.noreply.github.com>
2022-01-07 09:24:29 +0000

    Add comments

--- task3/data_cleaning_scripts/cleaning_script.R
+++ task3/data_cleaning_scripts/cleaning_script.R
@@ -4,10 +4,15 @@ raw_ship_data <- readxl::read_xls("raw_data/seabirds.xls", sheet = 1)
 raw_bird_data <- readxl::read_xls("raw_data/seabirds.xls", sheet = 2)
 
 
+# I only needed coordinates from this data set
+
 ship_data <- raw_ship_data %>% 
   janitor::clean_names() %>% 
   select(record_id, lat, long)
 
+
+# Only needed the names of the species and their count in each observation. I 
+# also removed instances where no birds were recorded.
 
 bird_data <- raw_bird_data %>% 
   janitor::clean_names() %>% 
@@ -16,6 +21,8 @@ bird_data <- raw_bird_data %>%
          species_abbreviation, count) %>% 
   filter(species_common_name != "[NO BIRDS RECORDED]")
 
+
+# Joined the 2 data sets for analysis
 
 all_data <- bird_data %>% 
   left_join(ship_data, by = "record_id") %>% 
